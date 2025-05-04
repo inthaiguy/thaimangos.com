528e804eebfdd01eb86c3eab654b9d574b75abda
Bryan <bangkok@gmail.com>
2025-05-03 19:51:19 -0700

    npm run dev
    Changed formating for this server use npm run dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        namdokmai: resolve(__dirname, 'namdokmai.html'),
+        khieosawoei: resolve(__dirname, 'khieosawoei.html'),
+        okrhongdamnoen: resolve(__dirname, 'okrhongdamnoen.html'),
+        raedpaet: resolve(__dirname, 'raedpaet.html'),
+        tongdam: resolve(__dirname, 'tongdam.html'),
+        quiz: resolve(__dirname, 'quiz.html'),
+        mangopersecond: resolve(__dirname, 'mangopersecond.html')
+      }
+    }
+  }
 })
